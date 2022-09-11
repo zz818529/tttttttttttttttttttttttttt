ccf7094c66a365f6ba7e857de6f469e7f6f35f50
Waruserbot <95116492+Waruserbot@users.noreply.github.com>
2022-09-11 17:20:21 +0530

    Initial commit

---
+++ TelethonHell/strings/devs.py
@@ -0,0 +1,27 @@
+DEVLIST = [
+    "1432756163",   # ForGo10God
+    "1874070588",   # ForGo10_God
+    "1533682758",   # Harry
+    "1344569458",   # iamgojoof6eyes
+]
+
+HARMFUL = [
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "session.save()",
+    "HELLBOT_SESSION",
+    "INSTAGRAM_SESSION",
+    "DATABASE_URL",
+    "BOT_TOKEN",
+    "API_HASH",
+    "APP_ID",
+    "HEROKU_API_KEY",
+]
+
+INVALID_UPLOAD = ["Hell-TBot.session-journal", "Hell-TBot.session", "insta/settings.json"]
