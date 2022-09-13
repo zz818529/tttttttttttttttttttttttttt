695fb6076a074431c3cc57fc8d98e5593d2d4af1
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-09-13 09:46:24 +0530

    Update __main__.py

--- TelethonHell/__main__.py
+++ TelethonHell/__main__.py
@@ -68,7 +68,7 @@ async def start_hellbot():
         C5 = await hells(Config.SESSION_5, H5, "SESSION_5")
         await tbot.start()
         total = C1 + C2 + C3 + C4 + C5
-        LOGS.info("••• HellBot Startup Completed •••")
+        LOGS.info("••• WaruserBot Startup Completed •••")
         LOGS.info("••• Starting to load Plugins •••")
         await plug_load("TelethonHell/plugins/*.py")
         await plug_channel(bot, Config.PLUGIN_CHANNEL)
