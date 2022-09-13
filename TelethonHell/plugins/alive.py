998443a7b88e670416d6bbeb63340172738a004c
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-09-13 10:58:02 +0530

    Update alive.py

--- TelethonHell/plugins/alive.py
+++ TelethonHell/plugins/alive.py
@@ -12,25 +12,23 @@ from . import *
 # -------------------------------------------------------------------------------
 
 ALIVE_TEMP = """
-<b><i>🔥🔥ɦɛʟʟɮօt ɨs օռʟɨռɛ🔥🔥</b></i>
-<i><b>↼ Øwñêr ⇀</i></b> : 『 <a href='tg://user?id={}'>{}</a> 』
-╭──────────────
-┣─ <b>» Telethon ~</b> <i>{}</i>
-┣─ <b>» Hêllẞø† ~</b> <i>{}</i>
-┣─ <b>» Sudo ~</b> <i>{}</i>
-┣─ <b>» Uptime ~</b> <i>{}</i>
-┣─ <b>» Ping ~</b> <i>{}</i>
-╰──────────────
-<b><i>»»» <a href='https://t.me/its_hellbot'>[ †hê Hêllẞø† ]</a> «««</i></b>
+<b><i>✨ᴡᴀʀᴜsᴇʀʙᴏᴛ ɪs ᴏɴʟɪɴᴇ✨</b></i>  
+<i><b> » ᴏᴡɴᴇʀ </i></b> :  <a href='tg://user?id={}'>{}</a> 
+ <b>» ᴛᴇʟᴇᴛʜᴏɴ :</b> <i>{}</i>
+ <b>» ᴡᴀʀᴜsᴇʀʙᴏᴛ :</b> <i>{}</i>
+ <b>» sᴜᴅᴏ :</b> <i>{}</i>
+ <b>» ᴜᴘᴛɪᴍᴇ :</b> <i>{}</i>
+ <b>» ᴘɪɴɢ :</b> <i>{}</i>
+<b><i>❤️‍🔥 <a href='https://t.me/waruserbot'>[ ᴡᴀʀᴜsᴇʀʙᴏᴛ ]</a> ❤️‍🔥</i></b>
 """
 
 msg = """{}\n
-<b><i>🏅 𝙱𝚘𝚝 𝚂𝚝𝚊𝚝𝚞𝚜 🏅</b></i>
-<b>Telethon ≈</b>  <i>{}</i>
-<b>Hêllẞø† ≈</b>  <i>{}</i>
-<b>Uptime ≈</b>  <i>{}</i>
-<b>Abuse ≈</b>  <i>{}</i>
-<b>Sudo ≈</b>  <i>{}</i>
+<b><i> ❤️‍🔥 ʙᴏᴛ sᴛᴀᴛᴜs ❤️‍🔥  </b></i>
+<b> ᴛᴇʟᴇᴛʜᴏɴ :</b>  <i>{}</i>
+<b> ᴡᴀʀᴜsᴇʀʙᴏᴛ :</b>  <i>{}</i>
+<b> ᴜᴘᴛɪᴍᴇ :</b>  <i>{}</i>
+<b> ᴀʙᴜsᴇ :</b>  <i>{}</i>
+<b> sᴜᴅᴏ :</b>  <i>{}</i>
 """
 # -------------------------------------------------------------------------------
 
@@ -52,7 +50,7 @@ async def up(event):
                 pic_list.append(c)
         PIC = random.choice(pic_list)
     else:
-        PIC = "https://telegra.ph/file/ea9e11f7c9db21c1b8d5e.mp4"
+        PIC = "https://telegra.ph/file/d871194e87181a0c0e5f2.jpg"
     end = datetime.datetime.now()
     ling = (end - start).microseconds / 1000
     omk = ALIVE_TEMP.format(ForGo10God, alive_name, tel_ver, hell_ver, is_sudo, uptime, ling)
@@ -66,11 +64,11 @@ async def up(event):
     await hell.delete()
 
 
-@hell_cmd(pattern="hell$")
+@hell_cmd(pattern="war$")
 async def hell_a(event):
     ForGo10God, HELL_USER, hell_mention = await client_id(event)
     uptime = await get_time((time.time() - StartTime))
-    am = gvarstat("ALIVE_MSG") or "<b>»» нєℓℓвσт ιѕ σиℓιиє ««</b>"
+    am = gvarstat("ALIVE_MSG") or "<b> WarUserBot is Online ««</b>"
     try:
         hell = await event.client.inline_query(Config.BOT_USERNAME, "alive")
         await hell[0].click(event.chat_id)
@@ -87,7 +85,7 @@ async def hell_a(event):
 CmdHelp("alive").add_command(
     "alive", None, "Shows the Default Alive Message"
 ).add_command(
-    "hell", None, "Shows Inline Alive Menu with more details."
+    "war", None, "Shows Inline Alive Menu with more details."
 ).add_warning(
     "✅ Harmless Module"
 ).add()
